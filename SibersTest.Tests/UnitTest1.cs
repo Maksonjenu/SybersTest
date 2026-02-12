975298a93ef2e84a06f820956cf23e5fd4e62b95
Fomas <mujik@test.ui>
2026-02-12 16:58:44 +0700

    add: test case for createing employee

--- SibersTest.Tests/UnitTest1.cs
+++ SibersTest.Tests/UnitTest1.cs
@@ -1,4 +1,9 @@
-﻿namespace SibersTest.Tests;
+﻿using Microsoft.EntityFrameworkCore;
+using SibersTest.Core.Entities;
+using SibersTest.Infrastructure.Data;
+using SibersTest.Services.Services;
+
+namespace SibersTest.Tests;
 
 [TestFixture]
 public class ServicesTests
@@ -9,8 +14,31 @@ public class ServicesTests
     }
 
     [Test]
-    public void Test()
+    [TestCase("John Doe", "john.doe@example.com", 1)]
+    public async Task CreateEmployee_ShoudSaveInDb(string fullName, string email, int expectedCount)
     {
-        Assert.Pass();
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+        .Options;
+
+        // 2. Юзаем контекст
+        using (var context = new ApplicationDbContext(options))
+        {
+            var service = new EmployeeService(context);
+            service.CreateAsync(new Services.DTOs.EmployeeFormDto
+            {
+                FullName = fullName,
+                Email = email
+            });
+
+            context.SaveChanges(); // Сохраняем изменения в базе данных
+
+        }
+        // 3. Проверяем в новом контексте (имитируем новый запрос)
+        using (var context = new ApplicationDbContext(options))
+        {
+            Assert.That(context.Employees.Count(), Is.EqualTo(expectedCount));
+        }
+
     }
 }
