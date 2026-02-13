e85f3ae20319dcf4f76ab63afff2deefe8d8932f
Fomas <mujik@test.ui>
2026-02-13 18:38:41 +0700

    add: extra test cases for creating employee

--- SibersTest.Tests/UnitTest1.cs
+++ SibersTest.Tests/UnitTest1.cs
@@ -15,8 +15,18 @@ public class ServicesTests
 
     [Test]
     [TestCase("John Doe", "john.doe@example.com", 1)]
+    [TestCase("John ", "john.doe@example.com", 0)]
+    [TestCase("John", "john.doe@example.com", 0)]
     [TestCase("", "", 0)]
+    [TestCase("John Doe", "", 0)]
+    [TestCase("", "john.doe@example.com", 0)]
+    [TestCase("", "johndoeexample.com", 0)]
+    [TestCase("", "johndoe@examplecom", 0)]
+    [TestCase("", "@.", 0)]
+
+
     public async Task CreateEmployee_ShoudSaveInDb(string fullName, string email, int expectedCount)
+
     {
         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
         .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
